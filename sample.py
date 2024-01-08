2c9aab74e69dfb8f06ae3e6a87936a4b682f128a
Dhanumjayapusala <121305810+Dhanumjayapusala@users.noreply.github.com>
2024-01-08 12:04:24 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,11 +4,9 @@ st.header("Types of cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.image("./cat",caption="Persian Cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.image("cat",caption="Persian cat",width=300,use_column_width=True)
+  st.write("Persian Cats are cute")
 with col2:
-  st.subheader("Ragdoll Cat")
-  st.image("./cat2",caption="Ragdoll Cat",width=300,use_column_width=True)
+  st.subheader("Ragdoll")
+  st.image("./cat2",caption="Ragdoll",width=300,use_column_width=True)
   st.write("Ragdoll cats are proud")
-
-  

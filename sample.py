1962b2809690d32b7ddbca18545a7e7e9c317ce0
Dhanumjayapusala <121305810+Dhanumjayapusala@users.noreply.github.com>
2024-01-08 11:48:30 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title='Cats')
+st.header("Types of cats")
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Persian Cat")
+  st.image("C:\Users\pusal\OneDrive\Pictures\cat",caption="Persian Cat",width=300,use_column_width=True)
+  st.write("Persian cats are cute")
+with col2:
+  st.subheader("Ragdoll Cat")
+  st.image("C:\Users\pusal\OneDrive\Pictures\cat2",caption="Ragdoll Cat",width=300,use_column_width=True)
+  st.write("Ragdoll cats are proud")
+
+  
